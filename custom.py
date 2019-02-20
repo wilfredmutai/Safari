3a56681ef13eb33e7d3b086c6e1da7e97de8c815
Wilfred Mutai <46667061+wilfredmutai10@users.noreply.github.com>
2019-02-20 21:49:14 +0300

    Add files via upload

---
+++ custom.py
@@ -0,0 +1,52 @@
+name = "wilfred"
+print(name)
+print("   /|")
+print("  / |")
+print(" /__|")
+
+
+character_name = "wilfred"
+character_gender = "Male"
+character_age = 18
+salary =  "1.2m"
+print("my name is " + character_name +",")
+print("I'm "+ character_gender +".")
+print ("My age is "+str(character_age)+ " years")
+print ("I earn a total of " +salary+ " per year")
+
+# arithmetic funtions
+print(3*(4+5))
+
+# modulus-outputs the remainder after division
+print(10%3)
+
+# For a number to be printed alongside words it should be converted to a string
+my_num = 5
+print(str(my_num) +" is my favourite number")
+
+# absolute value
+print(abs(-3))
+
+# largest number
+print(max(18, 20))
+
+# smallest number
+print(min(51, 89))
+
+# rounding off
+print(round(3.8))
+
+# powers of numbers
+print(pow(2, 12))
+
+# for more math functions
+from math import *
+# square root
+print(sqrt(81))
+
+# prompting the user to enter data
+name = input("Enter Your Name ")
+age = input("Enter Your age ")
+print("Hello "+name+ "!" "You are "+age+ "Years old.")
+
+
